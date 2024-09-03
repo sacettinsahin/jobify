81644745e2a20df4674b1e55244c707dfca9a60f
Sacettin Şahin <eng.sacettinsahin@gmail.com>
2024-09-03 15:10:15 +0300

    Jwt moved to response's cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,10 +22,16 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
 
-  const isValidUser=user && (await comparePassword(req.body.password, user.password));
+  const isValidUser =
+    user && (await comparePassword(req.body.password, user.password));
   if (!isValidUser) throw new UnauthenticatedError("invalid credentials.");
 
-  const token = createJWT({userId: user._id, role: user.role})
-
-  res.send({token: token});
+  const token = createJWT({ userId: user._id, role: user.role });
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+    secure: process.env.NODE_ENV === "production",
+  });
+  res.status(StatusCodes.OK).json({ message: "user logged in" });
 };
