04042bbbae3cba2ba6dc29a773cca3cdd3320734
Sacettin Şahin <eng.sacettinsahin@gmail.com>
2024-09-03 08:53:07 +0300

    Job model created.

---
+++ models/JobModel.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: String,
+    position: String,
+    jobStatus: {
+      type: String,
+      enum: ["interview", "declined", "pending"],
+      default: "pending",
+    },
+    jobTye: {
+      type: String,
+      enum: ["full-time", "part-time", "intership"],
+      default: "full-time",
+    },
+    jobLocaltion: {
+      type: String,
+      default: "my city",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Job", JobSchema);
