94eed8fa1591d51e6e2cc8aa69a12eba8feeb03e
Sacettin Şahin <eng.sacettinsahin@gmail.com>
2024-09-03 13:10:41 +0300

    User model created

---
+++ models/UserModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { ROLE } from "../utils/constants";
+
+const UserSchema = new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    lastName:{
+        type:String,
+        default: 'lastname'
+    },
+    location:{
+        type:String,
+        default: 'my city'
+    },
+    role:{
+        type:String,
+        enum: Object.values(ROLE),
+        default: ROLE.USER
+    }
+})
+
+export default mongoose.model("User", UserSchema);
